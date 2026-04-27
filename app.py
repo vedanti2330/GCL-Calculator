875a1c700c83022e44b96733e12edad69fa841cd
vedanti2330 <vedantipkashid@gmail.com>
2026-04-27 11:06:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import BytesIO
-from premium_engine import validate_customer_file, calculate_premium_row
+from premium_Calculation import validate_customer_file, calculate_premium_row
 
 st.set_page_config(page_title="GCL Premium Calculator", layout="wide")
 
